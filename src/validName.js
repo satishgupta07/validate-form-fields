886d0e163f2f9dd4be1334ba1fbd1bab784f9c2f
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2022-04-13 02:07:39 +0530

    Updated

--- src/validName.js
+++ src/validName.js
@@ -1,7 +1,9 @@
-export default function validName(name) {
+function validName(name) {
     if(/^[A-Za-z\s]+$/.test(name) == false) {
         return false;
     }else {
         return true;
     }
-};
+}
+
+module.exports = validName;
